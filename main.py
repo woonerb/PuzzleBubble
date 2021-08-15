cf403b825d05be3f072636d85dda2aaeb0edeb7a
woonerb <woonerb@daum.net>
2021-08-16 00:51:38 +0900

    배경이미지 생성

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 pygame.init()
 screen_width = 448
@@ -7,6 +8,12 @@ screen = pygame.display.set_mode((screen_width,screen_height))  # 화면 크기 
 pygame.display.set_caption("Puzzle Bobble")                     # 게임명 설정
 clock = pygame.time.Clock()
 
+#배경이미지 불러오기
+current_path = os.path.dirname(__file__)  #실행하는 py경로 가져오기
+background = pygame.image.load(os.path.join(current_path, "background.png"))
+
+
+
 running = True
 while running:
     clock.tick(60) #FPS를 60으로 설정
@@ -14,6 +21,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False                                     #반복문 탈출하도록 running변수 변경
+
+    screen.blit(background, (0,0))                              #(0,0) background 띄우기     
+    pygame.display.update()
+
+
 
             
 pygame.quit()
